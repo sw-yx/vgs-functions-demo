e494a06a4863b254b669844bcb76d51992973d09
sw-yx <swyx@dontemail.me>
2019-06-04 10:49:20 -0400

    fix oopsie

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ export default withSiteData(() => {
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", ...this.state })
+      body: encode({ "form-name": "insecureform", ...this.state })
     })
       .then(() => alert("Success!"))
       .catch((error) => alert(error))
