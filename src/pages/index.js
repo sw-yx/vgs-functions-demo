3efae71cdba1974e6ed82af1ce520759ac718265
sw-yx <swyx@dontemail.me>
2019-06-06 02:49:37 -0400

    use secure form

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ function App() {
       <h1>netlify simple vgs functions demo</h1>
       <h2>an insecure form</h2>
       {/* <form data-netlify method="post" name="insecureform"> */}
-      <form onSubmit={handleSubmit} name="insecureform">
+      {/* <form onSubmit={handleSubmit} name="insecureform">
         <p>
           <label>
             Your Name: <input type="text" name="name" onChange={changeHandler("name")} />
@@ -49,29 +49,29 @@ function App() {
         <p>
           <button type="submit">Send</button>
         </p>
+      </form> */}
+      <h2>a secure form</h2>
+
+      <form data-netlify method="post" name="secureform" secure>
+        <p>
+          <label>
+            Your Name: <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email: <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Secure Message: <textarea data-secure-field name="message" />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
       </form>
-      {/* <h2>a secure form</h2>
-  
-        <form data-netlify method="post" name="secureform" secure>
-          <p>
-            <label>
-              Your Name: <input type="text" name="name" />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your Email: <input type="email" name="email" />
-            </label>
-          </p>
-          <p>
-            <label>
-              Secure Message: <textarea data-secure-field name="message" />
-            </label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form> */}
     </div>
   )
 }
